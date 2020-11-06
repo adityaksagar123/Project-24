e75c74d2ff64c966904a6ab2355689660decec36
adityaksagar123 <71107661+adityaksagar123@users.noreply.github.com>
2020-11-05 19:50:41 -0500

    Add files via upload

--- Paper.js
+++ Paper.js
@@ -5,17 +5,23 @@ class Paper
         var options= 
         { 
             isStatic : false,
-            restitution : 0.3,
             friction : 0.5,
             density : 1.2
         }
+        this.radius = 30;
+        this.image = loadImage("SPRITES/paper.png");
         this.body = Bodies.circle(100,500, 15, options);
         World.add(world, this.body);
     }
 
     display()
     {
-       ellipse(this.body.position.x,this.body.position.y,30)
+        push()
+        translate(this.body.position.x, this.body.position.y); 
+        imageMode(RADIUS);
+        imageMode(CENTER)
+        image(this.image, 0, 0, this.radius, this.radius);
+        pop()
     }
 }
 
--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Render = Matter.Render;
 
 
 var box1, box2, box3;
@@ -18,6 +19,8 @@ function setup() {
 	createCanvas(800, 700);
 
 	
+
+
 	box1Sprite=createSprite(400+200, 650, 200,20);
 	box2Sprite=createSprite(300+200, 620, 20,100);
 	box3Sprite=createSprite(500+200, 620, 20,100);
@@ -32,27 +35,30 @@ function setup() {
 	box1  = Bodies.rectangle(400+200, 650, 200,20, {isStatic:true});
 	World.add(world, box1);
 
-	box2  = Bodies.rectangle(400+200, 650, 20,100, {isStatic:true});
+	box2  = Bodies.rectangle(300+200, 620, 20,100, {isStatic:true});
 	World.add(world, box2);
 
-	box3  = Bodies.rectangle(400+200, 650, 20,100, {isStatic:true});
+	box3  = Bodies.rectangle(500+200, 620, 20,100, {isStatic:true});
 	World.add(world, box3);
 	
 	ground = Bodies.rectangle(width/2, height-35, width, 10 , {isStatic:true} );
 	World.add(world, ground);
 	 
 	paper = new Paper();
+	var render = Render.create({ element: document.body, engine: engine, options: { width: 1200, height: 700, wireframes: false } }); 
+	Render.run(render);
 	Engine.run(engine);
+
   
 }
 
 
 function draw() {
+  Engine.update(engine);
   rectMode(CENTER);
-  background(0);
+  background(255);
   
   paper.display();
-  keyPressed();
   drawSprites();
  
 }
@@ -60,7 +66,7 @@ function draw() {
 function keyPressed() {
 	if (keyCode === DOWN_ARROW) {
 	   
-	Matter.Body.applyForce(paper.body,paper.body.position,{x:3, y:-4})
+	Matter.Body.applyForce(paper.body,paper.body.position,{x:30, y:-30})
  } 
 }
 
